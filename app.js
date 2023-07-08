aea90d127379425176da800ff521f9010e0c139a
pizzaYami <cyj9407@naver.com>
2023-07-08 18:07:46 +0900

    계산기 만들기

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-function sayHellow(name) {
-  console.log("hellow my name is" + ` ${name}`);
-}
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiply: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  square: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-sayHellow("yong");
-alert();
+calculator.add(3, 2);
+calculator.minus(3, 2);
+calculator.multiply(3, 2);
+calculator.divide(3, 2);
+calculator.square(3, 2);
