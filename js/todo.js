31ed866502dc65d3b43ee9a1e0819e010b3b41ab
pizzaYami <cyj9407@naver.com>
2023-07-16 11:16:39 +0900

    todo 삭제하는법

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-console.log(toDoForm);
+function deleltToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function createToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  span.innerText = newToDo;
+  button.innerText = "❌";
+  toDoList.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(button);
+  button.addEventListener("click", deleltToDo);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  createToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
