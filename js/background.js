5cf6e1a1aafd77572464b176daec39a312454461
pizzaYami <cyj9407@naver.com>
2023-07-20 08:22:15 +0900

    배경화면 만들기

--- js/background.js
+++ js/background.js
@@ -3,8 +3,8 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 const choiceImg = images[Math.floor(Math.random() * images.length)];
 
 const bgImg = document.createElement("img");
-
+bgImg.id = "bg";
 bgImg.src = `img/${choiceImg}`;
-bgImg.style = "width:500px";
+bgImg.appendChild(body);
 
 document.body.appendChild(bgImg);
