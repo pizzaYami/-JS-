b89124053dd40863365a37e330ab7bd9e57ec1f7
pizzaYami <cyj9407@naver.com>
2023-07-25 20:28:42 +0900

    배경만들고 날씨 우측하단 고정

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-const loginInput = document.querySelector("#login-form input");
-const loginForm = document.querySelector("#login-form");
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector(".greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
@@ -10,16 +12,13 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, username);
   paingGreeting(username);
 }
-function clickLink(event) {
-  event.preventDefault();
-  console.dir(event);
-}
+
 function paingGreeting(username) {
-  greeting.innerText = `Hellow ${savedUsername}`;
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem("usename");
+const savedUsername = localStorage.getItem("username");
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
--- js/background.js
+++ js/background.js
@@ -1,10 +1,5 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
-
 const choiceImg = images[Math.floor(Math.random() * images.length)];
 
-const bgImg = document.createElement("img");
-bgImg.id = "bg";
-bgImg.src = `img/${choiceImg}`;
-bgImg.appendChild(body);
-
-document.body.appendChild(bgImg);
+const bgImg = document.getElementById("bg");
+bgImg.style.backgroundImage = `url('img/${choiceImg}')`;
